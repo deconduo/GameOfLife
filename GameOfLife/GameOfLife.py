ff43596d2f3806ceea2ea65fb25234cab8357986
deconduo <deconduo@tcd.ie>
2013-06-10 21:50:02 +0100

    Added cell class and some functionality.

--- GameOfLife/GameOfLife.py
+++ GameOfLife/GameOfLife.py
@@ -1,12 +1,35 @@
 # Game Of Life
 
-# Classes
+'''Classes'''
 
-# Functions
+class cell(object):
+    def __init__(self, xPos, yPos, isAlive, willLive=False):
+        self.xPosInt = xPos
+        self.yPosInt = yPos
+        self.isAliveBool = isAlive
+        self.willLiveBool = willLive
+        
+    def checkNextStep(self):
+        if self.isAliveBool == True:
+            if getAdjacentCells(self) < 2 or getAdjacentCells(self) > 3:
+                self.willLiveBool = False
+        if self.isAliveBool == False:
+            if getAdjacentCells(self) == 3:
+                self.willLiveBool = True
+
+    def doNextStep(self):
+        self.isAliveBool = self.willLiveBool
+
+
+'''Functions'''
 
 # Prints the grid to the screen
-def printTheGrid():
-    pass
+def printTheGrid(cellList):
+    for cell in cellList:
+        if cell.isAliveBool == True:
+            print "*"
+        else:
+            print "-"
 
 # Calculates the number of adjacent living cells for a given cell
 def getAdjacentCells():
@@ -21,8 +44,14 @@ def nextStep():
     pass
 
 # Checks if the grid is empty
-def isEmptyGrid():
-    pass
+def isEmptyGrid(cellList):
+    n = False
+    for cell in cellList:
+        if cell.isAliveBool() == True:
+            n = True
+    return n
 
+'''Main Program'''
 
-# Main Program
+grid = [cell(0, 0, True), cell(0, 1, False), cell(1, 0, False), cell(1, 1, False)]
+printTheGrid(grid)
