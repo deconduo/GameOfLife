65540121422696a1e43878e87a75db576453e5bb
deconduo <deconduo@tcd.ie>
2013-06-10 20:06:53 +0100

    Added skeleton functions

--- GameOfLife/GameOfLife.py
+++ GameOfLife/GameOfLife.py
@@ -1,1 +1,28 @@
 # Game Of Life
+
+# Classes
+
+# Functions
+
+# Prints the grid to the screen
+def printTheGrid():
+    pass
+
+# Calculates the number of adjacent living cells for a given cell
+def getAdjacentCells():
+    pass
+
+# Places the given number of cells randomly on the board
+def randomlyPlaceLivingCells():
+    pass
+
+# Advances the board by one step
+def nextStep():
+    pass
+
+# Checks if the grid is empty
+def isEmptyGrid():
+    pass
+
+
+# Main Program
